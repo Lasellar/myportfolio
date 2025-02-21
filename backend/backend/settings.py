cab907176cd5e6c949750842b2730a4cef808f6b
Maksim Polushin <btnchsky.sad@mail.ru>
2025-02-21 23:18:58 +0300

    static 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -104,7 +104,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'backend_static/static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
