de1ba0788cb406005a8bfcc3d3478e7b59bfdade
Maksim Polushin <btnchsky.sad@mail.ru>
2025-02-21 23:05:43 +0300

    fixes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,13 +6,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'pages.views.page_404'
 handler500 = 'pages.views.page_500'
 handler502 = 'pages.views.page_502'
 handler503 = 'pages.views.page_503'
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
